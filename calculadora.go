5635092779a93a94e5dc53989ae650493da2c116
Cristian Clavijo <cristian982940@gmail.com>
2023-04-11 21:16:30 -0500

    Add files via upload

---
+++ calculadora.go
@@ -0,0 +1,40 @@
+package myCalculatorMod
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type calc struct{}
+
+func (c calc) operate(entrada string, operador string) int {
+	entradaLimpia := strings.Split(entrada, operador)
+	operador1 := parsear(entradaLimpia[0])
+	operador2 := parsear(entradaLimpia[1])
+	switch operador {
+	case "+":
+		return operador1 + operador2
+	case "-":
+		return operador1 - operador2
+	case "*":
+		return operador1 * operador2
+	case "/":
+		return operador1 / operador2
+	default:
+		return 0
+	}
+
+}
+
+func leerEntrada() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func parsear(entrada string) int {
+	operador, _ := strconv.Atoi(entrada)
+	return operador
+}
